1e915d10b28105c9dde6f96bce2e0a47ee640e1c
Aaron Francis <aaronfrancis99@gmail.com>
2023-04-10 16:17:40 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+def generate_word():
+    """generates a random 5-letter word"""
+    word = ''.join(random.sample)(string.ascii_lowercase,5)
+    return word
+
+def play_wordle():
+    """initialize target word by calling generate word and initialize guess"""
+    target_word = generate_word()
+    num_guess = 0
+
+    while True: 
+        guess = input('First guess (5 letter word): ')
+        if len(guess) !=5:
+            print('Your guess must be 5 letters long! ')
+            continue
+            
+        if guess == target_word:
+            print(f"Success! You guessed the correct word in {num_guess} tries")
+            break
+
+        feedback = []
+        for i in range(5):
+            if guess[i] == target_word[i]:
+                feedback.append(i)
+            elif guess[i] in target_word:
+                feedback.append()
